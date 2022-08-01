dc4b69af1d1ccac222d4f820a72e2f4e9551bda2
duckingnguyen <duckingnguyen189@gmail.com>
2022-08-01 15:20:41 +0700

    update tiktok

---
+++ src/components/Popper/Wrapper.js
@@ -0,0 +1,21 @@
+// import classNames from 'classnames/bind';
+// import styles from './Popper.module.scss';
+
+// const cx = classNames.bind(styles);
+
+// function Wrapper({ children }) {
+//     return <div className={cx('wrapper')}>{children}</div>;
+// }
+
+// export default Wrapper;
+
+import classNames from 'classnames/bind';
+import styles from './Popper.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Wrapper({ children }) {
+    return <div className={cx('wrapper')}>{children}</div>;
+}
+
+export default Wrapper;
